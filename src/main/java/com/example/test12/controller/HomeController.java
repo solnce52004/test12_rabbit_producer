960c7500a4279ece1858418be729630d2fc496cb
solnce52004 <solnce52004@yandex.ru>
2022-01-06 01:35:51 +0300

    <master> try enable ssl

---
+++ src/main/java/com/example/test12/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.test12.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Welcome! This is rabbitMQ producer!");
+    }
+}
